15eb8d2ca37f52629d469143c0d77ce648511c1a
cugone <cugone@gmail.com>
2015-09-09 22:02:19 -0500

    Sweeping changes. See Description.
    Removed unnecessary function prototypes.
    Set default value of GetMaxPlayCount to 0.
    Removed Debug functions.
    Added call to DisplayHandResults after Run if play_count is set to not
    automatic.

--- main.cpp
+++ main.cpp
@@ -16,18 +16,9 @@
 #include "Classes/CCard.h"
 #include "Classes/CDeck.h"
 
-void GameLoop(Game& game, Dealer& dealer, std::vector<Player>& players, unsigned int max_hand_size);
-void StartGame(Dealer& d);
-void Deal(Dealer& d, std::vector<Player>& players, unsigned int max_hand_size);
-void CalculatePlayerHandValues(std::vector<Player>& players);
-void DisplayPlayerHands(const std::vector<Player>& players, std::ostream& os = std::cout);
 void DisplayHandResults(const std::vector<Player>& players, std::ostream& os = std::cout);
 void WaitForEnter();
-void TakeCardsFromPlayers(Dealer& d, std::vector<Player>& players);
-void ResetGame(Dealer& d);
 bool PromptPlayAgain();
-void DebugDisplayHandValueTypes(const std::vector<Player>& players, std::ostream& os = std::cout);
-void DebugDisplayHandValueType(const Player& player, std::ostream& os = std::cout);
 unsigned int GetNumPlayers();
 unsigned int GetGameHandSize();
 unsigned long long GetMaxPlayCount();
@@ -45,6 +36,7 @@ int main() {
     unsigned long long playcount = 0;
     do {
         g.Run();
+        if(max_play_count <= 0) DisplayHandResults(players); //Only display hands to user if play is not automatic.
     } while(max_play_count > 0 ? (++playcount < max_play_count) : PromptPlayAgain());
 
     std::ofstream file;
@@ -81,7 +73,7 @@ unsigned long long GetMaxPlayCount() {
 
     tinyxml2::XMLDocument doc;
 
-    unsigned long long default_value = 100;
+    unsigned long long default_value = 0;
 
     auto load_result = doc.LoadFile("options.dat");
     if(load_result != tinyxml2::XML_NO_ERROR) return default_value;
@@ -106,20 +98,6 @@ unsigned long long GetMaxPlayCount() {
     return value;
 }
 
-
-void DebugDisplayHandValueTypes(const std::vector<Player>& players, std::ostream& os) {
-    for(auto& p : players) {
-        DebugDisplayHandValueType(p, os);
-        os << '\n';
-    }
-    os.flush();
-}
-
-void DebugDisplayHandValueType(const Player& player, std::ostream& os) {
-    auto& h = player.GetHand();
-    os << "0x" << std::setfill('0') << std::setw(4) << std::hex << h.GetValue() << std::setfill(' ') << '\t' << hand::TypeToString(h.GetHandType());
-}
-
 void DisplayHandResults(const std::vector<Player>& players, std::ostream& os) {
     auto p1hand = players[0].GetHand();
     auto p2hand = players[1].GetHand();
@@ -141,30 +119,9 @@ void WaitForEnter() {
 
 
 bool PromptPlayAgain() {
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cout << "Play again? (Y/N): ";
     char answer = '\0';
     std::cin >> answer;
     answer = toupper(answer);
     return answer == 'Y';
-}
-
-void DisplayPlayerHands(const std::vector<Player>& players, std::ostream& os) {
-    
-    std::string FaceValues[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
-    char SuitValues[] = { 'C', 'D', 'H', 'S' };
-
-    std::size_t player_number = 0;
-    for(auto& p : players) {
-
-        os << 'P' << (++player_number);
-        auto& h = p.GetHand();
-        for(auto& c : h) {
-            os << std::setw(3) << FaceValues[((c.GetFace() >> 4) - 2)];
-            os << SuitValues[static_cast<uint8_t>(std::log2(c.GetSuit()))];
-        }
-        os << '\n';
-    }
-    os.flush();
-
 }
